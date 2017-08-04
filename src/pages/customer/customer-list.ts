6c6b08e13d90b26e7b4363c1b789756df7a13e30
Shakir Hussain <shakir.h@HCL.COM>
2017-08-04 19:10:53 +0530

    lit

--- src/pages/customer/customer-list.ts
+++ src/pages/customer/customer-list.ts
@@ -11,15 +11,19 @@ export class CustomerListPage {
   constructor(public navCtrl: NavController, public barcodeScanner: BarcodeScanner) {
 
   }
- customerImageList:any=[
-   "assets/img/customer/images1.png",
-   "assets/img/customer/images2.png",
-   "assets/img/customer/images3.png",
-   "assets/img/customer/images4.jpg",
-   "assets/img/customer/images5.jpg",
-   "assets/img/customer/images6.jpg",
-   "assets/img/customer/images7.jpg",
-   "assets/img/customer/images8.png",
-   "assets/img/customer/images2.png",
- ]
+  customerImageList: any = [
+    "assets/img/customer/images1.png",
+    "assets/img/customer/images2.png",
+    "assets/img/customer/images3.png",
+    "assets/img/customer/images4.jpg",
+    "assets/img/customer/images5.jpg",
+    "assets/img/customer/images6.jpg",
+    "assets/img/customer/images7.jpg",
+    "assets/img/customer/images8.png",
+    "assets/img/customer/images2.png",
+  ]
+
+  gotoPage() {
+    this.navCtrl.push(HomePage);
+  }
 }
